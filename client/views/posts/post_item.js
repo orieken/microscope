335b3b6ae82cb13c39910ed2ad71cb1bbab8cc68
Oscar Rieken <oriekenjr@gmail.com>
2014-05-20 21:53:36 -0400

    Chapter 3 Discover Meteor completed

---
+++ client/views/posts/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by orieken on 5/20/14.
+ */
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
