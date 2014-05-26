94e98251d8f40797aa980ec794f8e2a8fedbd045
Oscar Rieken <oriekenjr@gmail.com>
2014-05-25 21:40:42 -0400

    now sorting posts, fixed typo in posts.js

--- collections/posts.js
+++ collections/posts.js
@@ -1,12 +1,12 @@
 Posts = new Meteor.Collection('posts');
 
 Meteor.methods({
-    post: function(postAttributes){
+    post: function (postAttributes) {
         var user = Meteor.user(),
-            postWithSameLink = Post.findOne({ url: postAttributes.url });
+            postWithSameLink = Posts.findOne({ url: postAttributes.url });
 
         if (!user)
-        throw new Meteor.Error(401, "You need to login to post new stories");
+            throw new Meteor.Error(401, "You need to login to post new stories");
 
         if (postAttributes.url && postWithSameLink) {
             throw new Meteor.Error(302, 'This Link has already been posted', postWithSameLink._id);
