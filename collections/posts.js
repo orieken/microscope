b8372da6f7ac95e16095e7a2b6e760306d4e530f
Oscar Rieken <oriekenjr@gmail.com>
2014-05-25 21:47:07 -0400

    added check for empty headline

--- collections/posts.js
+++ collections/posts.js
@@ -8,6 +8,9 @@ Meteor.methods({
         if (!user)
             throw new Meteor.Error(401, "You need to login to post new stories");
 
+        if (!postAttributes.title)
+            throw new Meteor.Error(422, 'Please fill in a headline');
+
         if (postAttributes.url && postWithSameLink) {
             throw new Meteor.Error(302, 'This Link has already been posted', postWithSameLink._id);
         }
