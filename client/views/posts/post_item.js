4eb9a5c046e136d92f64b1801ce92077f762babc
Oscar Rieken <oriekenjr@gmail.com>
2014-05-26 00:00:38 -0400

    added edit posts form

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -2,6 +2,9 @@
  * Created by orieken on 5/20/14.
  */
 Template.postItem.helpers({
+    ownPost: function() {
+        return this.userId == Meteor.userId();
+    },
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
