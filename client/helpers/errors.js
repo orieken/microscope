22ff6bbe87145822e785afee6195360018a00b33
Oscar Rieken <oriekenjr@gmail.com>
2014-05-26 20:05:17 -0400

    added basic error reporting

---
+++ client/helpers/errors.js
@@ -0,0 +1,8 @@
+/**
+ * Created by orieken on 5/26/14.
+ */
+Errors = new Meteor.Collection(null);
+
+throwError = function(message) {
+    Errors.insert({ message: message })
+}
---
+++ client/views/includes/errors.js
@@ -0,0 +1,8 @@
+/**
+ * Created by orieken on 5/26/14.
+ */
+Template.errors.helpers({
+    errors: function() {
+        return Errors.find();
+    }
+});
